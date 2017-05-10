9f0ec32a880e70cc5c2a0a9adda2b8e38dffbf0c
SeanROlszewski <sean.r.olszewski@gmail.com>
2017-05-10 18:42:53 -0400

    Adds URLSessionSpy example

---
+++ Client/Client.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+enum Result <T> {
+    case success(T)
+    case failure(NetworkError)
+}
+
+enum NetworkError: Error {
+    case unspecified
+}
+
+protocol URLSessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTask
+}
+
+struct NetworkClient {
+    let session: URLSessionProtocol
+    init(with session: URLSessionProtocol) {
+        self.session = session
+    }
+    
+    func get(resourceAt url: URL, completion: @escaping (Result<Data>) -> ()) {
+        let dataTask = session.dataTask(with: url) { (data: Data?, urlResponse: URLResponse?, error: Error?) in
+    
+            guard error == nil else {
+                completion(Result<Data>.failure(NetworkError.unspecified))
+                return
+            }
+            
+            completion(Result<Data>.success(data ?? Data()))
+        }
+        
+        dataTask.resume()
+    }
+}
--- ClientTests/ClientTests.swift
+++ ClientTests/ClientTests.swift
@@ -1,36 +1,91 @@
-//
-//  ClientTests.swift
-//  ClientTests
-//
-//  Created by seandorian on 5/6/17.
-//  Copyright © 2017 Sean Olszewski. All rights reserved.
-//
-
-import XCTest
+import Quick
+import Nimble
 @testable import Client
 
-class ClientTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class ClientSpec: QuickSpec {
+    override func spec() {
+        describe("") {
+            describe("invokes the completion handler") {
+                it("with data when it succeeds") {
+                    let session = URLSessionSpy()
+                    let sessionTask = URLSessionDataTaskSpy()
+                    session.dataTaskToReturn = sessionTask
+                    session.completionHandlerArguments = (data: Data(), response: nil, error: nil)
+                    
+                    let client = NetworkClient(with: session)
+                    var requestResult: Result<Data>?
+
+                    client.get(resourceAt: URL(string: "http://www.example.com")!) { result in
+                        requestResult = result
+                    }
+                    
+                    guard let result = requestResult,
+                        case let Result.success(data) = result,
+                        data == Data() else {
+                            fail("expected to get data, got \(String(describing: requestResult)) instead")
+                            return 
+                    }
+                }
+                
+                it("with an error when it fails") {
+                    let session = URLSessionSpy()
+                    let sessionTask = URLSessionDataTaskSpy()
+                    session.dataTaskToReturn = sessionTask
+                    session.completionHandlerArguments = (data: nil, response: nil, error: NetworkError.unspecified)
+                    
+                    let client = NetworkClient(with: session)
+                    var requestResult: Result<Data>?
+                    
+                    client.get(resourceAt: URL(string: "http://www.example.com")!) { result in
+                        requestResult = result
+                    }
+                    
+                    guard let result = requestResult,
+                        case let Result.failure(error) = result,
+                        error == NetworkError.unspecified else {
+                            fail("expected to get data, got \(String(describing: requestResult)) instead")
+                            return
+                    }
+                }
+            }
+            
+            it("makes a network request with the URL") {
+                let session = URLSessionSpy()
+                let sessionTask = URLSessionDataTaskSpy()
+                session.dataTaskToReturn = sessionTask
+                session.completionHandlerArguments = (data: nil, response: nil, error: nil)
+                
+                let client = NetworkClient(with: session)
+                let expectedURL = URL(string: "http://www.example.com")!
+                
+                client.get(resourceAt: URL(string: "http://www.example.com")!) { _ in }
+                expect(session.dataTaskArguments?.url).to(equal(expectedURL))
+                expect(sessionTask.resumeWasInvoked).to(beTrue())
+            }
         }
     }
+} 
+extension URLSession: URLSessionProtocol {}
+
+class URLSessionSpy: URLSessionProtocol {
+    var dataTaskArguments: (url: URL, completionHandler: (Data?, URLResponse?, Error?) -> ())?
+    var dataTaskToReturn: URLSessionDataTask!
+    var completionHandlerArguments: (data: Data?, response: URLResponse?, error: Error?)!
     
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        dataTaskArguments = (url: url, completionHandler: completionHandler)
+        
+        completionHandler(completionHandlerArguments.data,
+                          completionHandlerArguments.response,
+                          completionHandlerArguments.error)
+        
+        return dataTaskToReturn
+    }
+}
+
+class URLSessionDataTaskSpy: URLSessionDataTask {
+    var resumeWasInvoked = false
+    override func resume() {
+        resumeWasInvoked = true
+    }
 }
